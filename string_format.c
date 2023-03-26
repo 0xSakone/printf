86925000a72027cd27b4bb6539481285408664b5
0xSakone <abdramanesakone4@gmail.com>
2023-03-26 10:37:22 +0200

    🐛 Fix betty styling

--- string_format.c
+++ string_format.c
@@ -7,16 +7,17 @@
  */
 int string_format(char *s)
 {
-    int c = 0, i = 0;
-    char cc;
-    if (s == NULL)
-        s = "(null)";
-    while (s[i] != '\0')
-    {
-        cc = s[i];
-        cprintf(&cc, 1);
-        i++;
-        c++;
-    }
-	return c;
+	int c = 0, i = 0;
+	char cc;
+
+	if (s == NULL)
+		s = "(null)";
+	while (s[i] != '\0')
+	{
+		cc = s[i];
+		cprintf(&cc, 1);
+		i++;
+		c++;
+	}
+	return (c);
 }
