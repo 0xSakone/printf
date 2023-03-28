606a17beb79c0d32e5ce3bea13010d0e41991265
0xSakone <abdramanesakone4@gmail.com>
2023-03-28 04:08:05 +0200

    Update 1.381

--- _printf.c
+++ _printf.c
@@ -41,7 +41,7 @@ int _printf(const char * const format, ...)
 	char output[2048];
 	va_list args;
 
-	if (!format)
+	if (format[0] == '%' && _strlen((char *)format) == 1)
 		return (-1);
 	va_start(args, format);
 	fm = (char *)format;
