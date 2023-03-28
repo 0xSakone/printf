ba4b2dde8a213949c1c263baac121167e75c16d0
0xSakone <abdramanesakone4@gmail.com>
2023-03-28 05:00:50 +0200

    Add number 1.0

--- print_number.c
+++ print_number.c
@@ -1,4 +1,25 @@
 #include "main.h"
+#include <limits.h>
+
+/**
+ * print_number_helper - print number
+ * @number: number to print
+ * Return: number of caracter
+ */
+int print_number_helper(int number)
+{
+	int len = 0;
+	char c[1];
+
+	if (number / 10)
+		len += print_number_helper(number / 10);
+
+	c[0] = (char)(number % 10) + '0';
+	cprintf(c, 1);
+	len++;
+
+	return (len);
+}
 
 /**
  * print_number - print number
@@ -8,23 +29,23 @@
 int print_number(int number)
 {
 	int len = 0;
-	char c[1];
 
 	if (number < 0)
 	{
 		cprintf("-", 1);
-		number = -number;
 		len++;
-	}
 
-	if (number / 10)
-	{
-		len += print_number(number / 10);
+		if (number == INT_MIN)
+		{
+			len += print_number_helper(-(number / 10));
+			cprintf("8", 1);
+			len++;
+		}
+		else
+			len += print_number_helper(-number);
 	}
-
-	c[0] = (char)(number % 10) + '0';
-	cprintf(c, 1);
-	len++;
+	else
+		len += print_number_helper(number);
 
 	return (len);
 }
