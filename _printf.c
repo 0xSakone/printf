9d5fb7e3b5faa868c0e11dfc4e706b670399b1bf
0xSakone <abdramanesakone4@gmail.com>
2023-03-30 14:34:31 +0200

    Fix betty style printf

--- _printf.c
+++ _printf.c
@@ -14,25 +14,20 @@ int _1bit(int *count, char c)
 }
 
 /**
- * _printf - printf function simulator
- * @format: text format
- * Return: number of character
+ * dispatch - printf function simulator
+ * @count: counter
+ * @args: argument
+ * @fm: character
+ * Return: formater array
  */
-int _printf(const char * const format, ...)
+int dispatch(int *count, va_list args, char *fm)
 {
-	int count = 0, _dispatcher = 0, i = 0, find = 0;
-	char *fm;
-	va_list args;
+	int _dispatcher = 0, i = 0, find = 0;
+
 	func _f[] = {
 		{'c', character_format}, {'s', string_format}, {'d', number_format},
 		{'i', number_format}, {'b', binary_format}, {'%', perc_format}, {'\0', NULL}
 	};
-
-	if (!format)
-		return (-1);
-	va_start(args, format);
-	
-	fm = (char *)format;
 	while (*fm)
 	{
 		if (*fm == '%')
@@ -43,7 +38,7 @@ int _printf(const char * const format, ...)
 			{
 				if (*fm == _f[i].c)
 				{
-					_dispatcher = _f[i].call(&count, args);
+					_dispatcher = _f[i].call(count, args);
 					find = 1;
 					if (_dispatcher == -1)
 						return (-1);
@@ -51,14 +46,36 @@ int _printf(const char * const format, ...)
 			}
 			if (find == 0)
 			{
-				_1bit(&count, '%');
-				_1bit(&count, *fm);
+				_1bit(count, '%');
+				_1bit(count, *fm);
 			}
 		}
 		else
-			_1bit(&count, *fm);
+			_1bit(count, *fm);
 		fm++;
 	}
+	return (*count);
+}
+
+/**
+ * _printf - printf function simulator
+ * @format: text format
+ * Return: number of character
+ */
+int _printf(const char * const format, ...)
+{
+	char *fm;
+	int count = 0;
+	va_list args;
+
+	if (!format)
+		return (-1);
+	va_start(args, format);
+
+	fm = (char *)format;
+	count = dispatch(&count, args, fm);
+	if (count == -1)
+		return (-1);
 	va_end(args);
 	return (count);
 }
